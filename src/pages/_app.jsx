188d91a6d7570b2040706e5629a116846fa093f5
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-16 17:55:06 +0530

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiConfig } from 'wagmi';
-import { wagmiConfig, chains } from '../lib/wagmi'; // हमने जो wagmi.js बनाई थी
+import { wagmiConfig, chains } from '../lib/wagmi';
 
 function MyApp({ Component, pageProps }) {
   return (
